6b190f1b1dab2aef3b459fbaa1bced7cce77ba0d
ThomasTsao0704 <120099666+ThomasTsao0704@users.noreply.github.com>
2025-08-28 18:35:14 +0800

    Create sw.js

---
+++ App19/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'procure-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      if(req.method === 'GET') {
+        const resClone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(req, resClone));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
