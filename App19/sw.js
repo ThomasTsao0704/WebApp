4b57257ffb2e8c22be22acf0ac3e045c19d8b6aa
ThomasTsao0704 <120099666+ThomasTsao0704@users.noreply.github.com>
2025-08-29 15:25:38 +0800

    Add files via upload

--- App19/sw.js
+++ App19/sw.js
@@ -1,34 +1,37 @@
-const CACHE = 'procure-pwa-v1';
+const CACHE = 'procure-pwa-v2'; // 改版號即可刷新快取
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-512-maskable.png'
+    './',
+    './index.html',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+    e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))).then(() => self.clients.claim())
-  );
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      if(req.method === 'GET') {
-        const resClone = res.clone();
-        caches.open(CACHE).then(cache => cache.put(req, resClone));
-      }
-      return res;
-    }).catch(() => caches.match('./index.html')))
-  );
+    const req = e.request;
+    if (req.method !== 'GET') return;
+    e.respondWith(
+        caches.match(req).then(cached => {
+            const fetcher = fetch(req).then(res => {
+                if (res && res.status === 200 && new URL(req.url).origin === location.origin) {
+                    const clone = res.clone();
+                    caches.open(CACHE).then(c => c.put(req, clone));
+                }
+                return res;
+            }).catch(() => cached || caches.match('./index.html'));
+            return cached || fetcher;
+        })
+    );
 });
