43bcbc09efb2a89dc147be59a26aa2e0bd22c7bf
ThomasTsao0704 <120099666+ThomasTsao0704@users.noreply.github.com>
2025-09-02 15:50:47 +0800

    Update vite.config.ts

--- App23/vite.config.ts
+++ App23/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/WebApp/' // ← 改成你的 Repo 名稱
+  plugins: [react()],
+  base: '/WebApp/App23/'   // ★ 關鍵：與你的子資料夾完整相同（前後都要斜線）
 })
